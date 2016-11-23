5c6c8d27c2b6bbca7cf93c8aeafb61a3f1a8bea6
Tom Kliethermes <kliethermes.tom@gmail.com>
2016-11-23 14:37:42 -0600

    Help and about activities

---
+++ app/src/main/java/com/quaap/goaltender/AboutActivity.java
@@ -0,0 +1,35 @@
+package com.quaap.goaltender;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        try {
+            PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            String version = pInfo.versionName;
+
+            System.out.println(getResources().getString(R.string.app_name));
+
+            TextView txtversion = (TextView)findViewById(R.id.txtversion);
+            txtversion.setText(version);
+
+
+            TextView txtnamelink = (TextView)findViewById(R.id.txtnamelink);
+            txtnamelink.setMovementMethod(LinkMovementMethod.getInstance());
+            txtnamelink.setText(Html.fromHtml(getString(R.string.name_link)));
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
